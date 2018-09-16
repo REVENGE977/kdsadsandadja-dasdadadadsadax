10784e1b6b2b12065e3d39afdc46442bca63b403
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-09-16 10:19:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,19 +98,7 @@ if(!incidentchannel) return message.channel.send("Can't find incidents channel."
   });
 
 
-var anti_spam = require("discord-anti-spam");
- 
-antispam(bot, {
-  warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
-  maxBuffer: 10, // Maximum amount of messages allowed to send in the interval time before getting banned.
-  interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
-  warningMessage: "stop spamming or I'll whack your head off.", // Warning message send to the user indicating they are going to fast.
-  banMessage: "has been banned for spamming, anyone else?", // Ban message, always tags the banned user in front of it.
-  maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
-  maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
-  deleteMessagesAfterBanForPastDays: 7 // Delete the spammed messages after banning for the past x days.
-});
- 
+
 
 
 client.login(process.env.BOT_TOKEN);
