4f70460ec3f4b5d320b26f46a949a6196a3b9c9e
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-09-18 13:04:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -243,6 +243,22 @@ client.on('message',async message => {
     }
   });
 
-
+client.on('message',async message => {
+    if(message.content.startsWith(prefix + "setDays")) {
+    if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
+    if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
+    message.channel.send('✅| **تم عمل الروم بنجاح**');
+    message.guild.createChannel(`${moment().format('dddd')}` , 'voice').then(c => {
+      console.log(`Day channel setup for guild: \n ${message.guild.name}`);
+      c.overwritePermissions(message.guild.id, {
+        CONNECT: false,
+        SPEAK: false
+      });
+      setInterval(function() {
+        c.setName(`${moment().format('dddd')}`);
+      },1000);
+    });
+    }
+  });
 
 client.login(process.env.BOT_TOKEN);
