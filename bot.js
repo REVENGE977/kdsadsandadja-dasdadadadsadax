db885e505db9f7d94081f5ef8dece991fe84a4d4
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-09-18 13:20:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -249,14 +249,14 @@ client.on('message',async message => {
     if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
     if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
     message.channel.send('✅| **تم عمل الروم بنجاح**');
-    message.guild.createChannel(`${moment().format('dddd')}` , 'voice').then(c => {
+    message.guild.createChannel(`Day : ${moment().format('dddd')}` , 'voice').then(c => {
       console.log(`Day channel setup for guild: \n ${message.guild.name}`);
       c.overwritePermissions(message.guild.id, {
         CONNECT: false,
         SPEAK: false
       });
       setInterval(function() {
-        c.setName(`${moment().format('dddd')}`);
+        c.setName(`Day : ${moment().format('dddd')}`);
       },1000);
     });
     }
