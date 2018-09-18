dc27f5140fc8e0d9fc1bb1df6b2b042602accc4f
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-09-18 13:17:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -244,6 +244,7 @@ client.on('message',async message => {
   });
 
 client.on('message',async message => {
+  var moment = require('moment');
     if(message.content.startsWith(prefix + "setDays")) {
     if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
     if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
