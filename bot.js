cbb0555a26bd365efb75e2de1d6311acc7460bfb
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-09-18 10:16:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,14 +146,14 @@ client.on('message',async message => {
     if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
     if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
     message.channel.send('✅| **تم عمل الروم بنجاح**');
-    message.guild.createChannel(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
+    message.guild.createChannel(`🎤 Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
       console.log(`Voice online channel setup for guild: \n ${message.guild.name}`);
       c.overwritePermissions(message.guild.id, {
         CONNECT: false,
         SPEAK: false
       });
       setInterval(function() {
-        c.setName(`Voice Online : 「${message.guild.members.filter(m => m.voiceChannel).size}」`)
+        c.setName(`🎤 Voice Online : 「${message.guild.members.filter(m => m.voiceChannel).size}」`)
       },1000);
     });
     }
@@ -164,14 +164,14 @@ client.on('message',async message => {
     if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
     if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
     message.channel.send('✅| **تم عمل الروم بنجاح**');
-    message.guild.createChannel(`Members Count : [ ${message.guild.members.size} ]` , 'voice').then(c => {
+    message.guild.createChannel(`👪 Members Count : [ ${message.guild.members.size} ]` , 'voice').then(c => {
       console.log(`Count Members channel setup for guild: \n ${message.guild.name}`);
       c.overwritePermissions(message.guild.id, {
         CONNECT: false,
         SPEAK: false
       });
       setInterval(function() {
-        c.setName(`Members Count : 「${message.guild.members.size}」`)
+        c.setName(`👪 Members Count : 「${message.guild.members.size}」`)
       },1000);
     });
     }
