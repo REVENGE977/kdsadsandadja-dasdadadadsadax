6ea2eb6ee3fc33080e4902c243838469a45e06af
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-09-30 13:41:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1300,4 +1300,25 @@ function play(guild, song) {
 }
 });
 
+client.on('message', message =>{
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    let prefix = '!!';
+
+if(cmd === `${prefix}suggest`) {
+    var suggestMessage = message.content.substring(8)
+    let suggestEMBED = new Discord.RichEmbed()
+    .setColor(3447003)
+    .setTitle("New suggest just added!!")
+    .setDescription(`**${suggestMessage}**`)
+    .setFooter(`Suggested By : ${message.author.tag}`);
+    message.delete().catch(O_o=>{}) 
+    let suggests = message.guild.channels.find(`name`, "suggests");
+    if (!suggests) return message.channel.send("You should make A **suggests** channel!")
+    suggests.send(suggestEMBED);
+}
+
+});
+
 client.login(process.env.BOT_TOKEN);
