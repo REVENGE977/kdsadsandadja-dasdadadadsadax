65f2713141797b557696d69fe62822cbd4ed5d22
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-10-10 20:05:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1582,7 +1582,7 @@ const w = ['./w1.png'];
                         ctx.fontSize = '20px';
                         ctx.fillStyle = "#f1f1f1";
                         ctx.textAlign = "center";
-                        ctx.fillText(`welcome to Brix`, 300, 130);
+                        ctx.fillText(`Welcome To MarsMC Network`, 300, 130);
                        
                         ctx.font = "bold 12px Arial";
                         ctx.fontSize = '20px';
