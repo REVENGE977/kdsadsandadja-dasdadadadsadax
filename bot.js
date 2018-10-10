f4cccf12ca72a5268c67e8e47bc9030854ed0100
_ShaDoW_MaN_ <32039975+ShadwX57@users.noreply.github.com>
2018-10-10 18:12:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1552,7 +1552,6 @@ client.on('message',message =>{
         )}
 })
 
-const fs = require('fs');
 const moment = require('moment');
 const jimp = require('jimp');
 const Canvas = require('canvas');
